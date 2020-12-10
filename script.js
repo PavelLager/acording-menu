76817e4f8f2fb68313e52c8f7e7b03e857251848
PavelLager <74252968+PavelLager@users.noreply.github.com>
2020-12-10 17:58:52 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,57 @@
 debugger
 const APP_acordions = document.querySelector('.acordions');
 
-let item = [{id: 1, title: 'asdf', description: 'bla bla bla'}];
+let item = [{id: 1, title: 'asdf', description: 'bla bla bla'},
+{id: 2, title: 'asdf', description: 'bla bla bla'},
+{id: 3, title: 'asdf', description: 'bla bla bla'}];
+let logs = [{id: 0, count: 0}]
 
 function addAco() {
-    APP_acordions.innerHTML = `
+    return `
     <div class="acordion">
-    <div class="acordion_title">${item.title}
-        <button class="acordion_button"></button>
+    <div class="acordion_title">
+        <button class="acordion_button" data-id='${item.id}'>O</button>
+
+        ${item.title}
     </div>
     <div class="acordion_discription">${item.description}</div>
 </div>`
 }
 
-addAco();
+function getAco() {
+    return item.map(() => addAco()).join('');
+  }
 
 
+function main(){
+    APP_acordions.innerHTML = getAco();
+}
+main();
 
-document.querySelector('.acordion_button').addEventListener('click', function(){
+let a = document.querySelectorAll('.acordion_button');
+a.addEventListener('click', function(){
     document.querySelector('.acordion_discription').style.display = 'flex';
+    document.querySelector('.acordion_button').style.display = 'none';
+    document.querySelector('.acordion_button2').style.display = 'block';
 })
+
+
+
+
+
+
+document.querySelectorAll('.acordion_button').addEventListener('click', function(){
+    document.querySelectorAll('.acordion_discription').style.display = 'flex';
+    document.querySelectorAll('.acordion_button').style.display = 'none';
+    document.querySelectorAll('.acordion_button2').style.display = 'block';
+
+})
+
+document.querySelectorAll('.acordion_button2').addEventListener('click', function(){
+    document.querySelectorAll('.acordion_discription').style.display = 'none';
+    document.querySelectorAll('.acordion_button').style.display = 'block';
+    document.querySelectorAll('.acordion_button2').style.display = 'none';
+})
+
+
+
